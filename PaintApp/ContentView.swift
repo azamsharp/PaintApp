891b8619db7146b3085f256ca5c92c8b45788d34
Mohammad Azam <azamsharp@gmail.com>
2021-12-07 07:50:57 -0600

    Initial Commit

---
+++ PaintApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PaintApp
+//
+//  Created by Mohammad Azam on 12/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PaintApp/PaintAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaintAppApp.swift
+//  PaintApp
+//
+//  Created by Mohammad Azam on 12/7/21.
+//
+
+import SwiftUI
+
+@main
+struct PaintAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
